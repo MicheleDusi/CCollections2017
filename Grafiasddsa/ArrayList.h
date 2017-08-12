b93ee81b336ca95e6bc855e91be52dbe619ce3c8
MicheleDusi <m.dusi007@studenti.unibs.it>
2017-08-12 23:41:34 +0200

    Huge refactoring and re-organization of functions order.

--- Grafiasddsa/ArrayList.h
+++ Grafiasddsa/ArrayList.h
@@ -9,35 +9,65 @@ typedef struct arraylist {
 	void** array;
 } arraylist;
 
+// Initializing List
 arraylist* al_initListWithCapacity(int cap);
 arraylist* al_initList(); // OK
+
+// Size
+int al_getListSize(arraylist* l);
+
+// Cancelling List
 void al_cleanList(arraylist* l); // OK
 void al_purgeList(arraylist* l); // OK
-void al_insertElementFirst(arraylist* l, void* new_element_data);
-void al_insertElementLast(arraylist* l, void* new_element_data); // OK
+
+// Inserting Elements
+void al_insertFirstElement(arraylist* l, void* new_element_data);
+void al_insertLastElement(arraylist* l, void* new_element_data); // OK
 void al_insertElementAtPosition(arraylist* l, void* new_element_data, int pos);
+
+// Deleting Elements
 void al_deleteFirstElement(arraylist* l);
 void al_deleteLastElement(arraylist* l);
 void al_deleteElementAtPosition(arraylist* l, int pos);
-void al_deleteElementsByConditions(arraylist* l, bool (*condition)(void*));
-void* al_getHeadContent(arraylist* l);
-void* al_extractHeadContent(arraylist* l);
+void al_deleteElementsByCondition(arraylist* l, bool (*condition)(void*));
+
+// Purging Elements
+void al_purgeFirstElement(arraylist* l); /// TODO
+void al_purgeLastElement(arraylist* l); /// TODO
+void al_purgeElementAtPosition(arraylist* l, int pos); /// TODO
+void al_purgeElementsByCondition(arraylist* l, bool (*condition)(void*)); // NEW
+
+// Getting Elements
+void* al_getFirstElement(arraylist* l);
+void* al_getLastElement(arraylist* l);
+void* al_getElementAtPosition(arraylist* l, int pos);
+arraylist* al_getElementsByCondition(arraylist* l, bool (*condition)(void*)); /// TODO
+arraylist* al_getSubList(arraylist* l, int start_pos, int end_pos);
+
+// Extracting Elements
+void* al_extractFirstElement(arraylist* l);
+void* al_extractLastElement(arraylist* l); /// TODO
 void* al_extractElementAtPosition(arraylist* l, int pos);
 arraylist* al_extractElementsByCondition(arraylist* l, bool (*condition)(void*)); // NEW
-void al_purgeElementsByCondition(arraylist* l, bool (*condition)(void*)); // NEW
-void* al_getTailContent(arraylist* l);
-void* al_getElementContentAtPosition(arraylist* l, int pos);
-int al_getListSize(arraylist* l);
+
+// Searching Elements
+bool al_containsElement(arraylist* l, void* element_content); /// TODO
+bool al_containsElementByCondition(arraylist* l, bool (*condition)(void*));
+int al_countElementsByCondition(arraylist* l, bool (*condition)(void*)); /// TODO
 int al_getElementPosition(arraylist* l, void* element_content);
-bool al_containsElement(arraylist* l, bool (*condition)(void*));
-void al_swapTwoElements(arraylist* l, int pos1, int pos2);
+
+// Cloning and Managing List
 arraylist* al_cloneOrderedList(arraylist* l, void* (*clone)(void*));
+arraylist* al_cloneSubList(arraylist* l, int start_pos, int end_pos, void* (*clone)(void*));
 arraylist* al_concatenateTwoLists(arraylist* l1, arraylist* l2, void* (*clone)(void*));
+
+// Sorting List
+void al_swapTwoElements(arraylist* l, int pos1, int pos2);
+void al_sortByOrder(arraylist* l, int (*compare)(void*, void*)); // OK
 void* al_getMinimumContent(arraylist* l, int (*compare)(void*, void*));
 void* al_getMaximumContent(arraylist* l, int (*compare)(void*, void*));
-arraylist* al_getSubList(arraylist* l, int start_pos, int end_pos);
-arraylist* al_cloneSubList(arraylist* l, int start_pos, int end_pos, void* (*clone)(void*));
-void al_sortByOrder(arraylist* l, int (*compare)(void*, void*)); // OK
+
+// Visualizing List
 char* al_listToString(arraylist* l, char* (*toStringFunction)(void*)); // OK
 
 #endif
